5fc65ebffc7b65507bedece4d21f9bcdd3936006
LeoCorreiaBiloti <134016427+LeoCorreiaBiloti@users.noreply.github.com>
2025-01-13 19:33:52 -0300

    Ajuste de formatação e criação de diretórios

---
+++ Floresa-JAVA/demo/demo/src/main/java/com/example/demo/User.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+public class User {
+
+
+
+    @Entity
+    public class User{
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private  Long id;
+        private String name;
+        private String email;
+
+        public Long getId(){return id; }
+        public void setId(Long id) {this.id = id; }
+
+        public String getName(){return name; }
+        public void setSName(String name) {this.name = name; }
+
+        public String getEmail(){return email; }
+        public void setId(String email) {this.email = email; }
+
+
+    }
+}
--- Floresa/demo/demo/src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository {
-
-    public interface UserRepository extends JpaRepository<User, Long> {}
-}
